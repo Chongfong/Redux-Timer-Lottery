abe2f91a96f66e29f71b62ee68dc0c908c56be00
churin <wl00796865@gmail.com>
2024-05-08 17:20:14 +0800

    refactor: refactor counter
    (cherry picked from commit d640803ab1deac00ef15a219143b7a4134d1897f)

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState, useEffect, useCallback, memo,
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import {
-  update, countDown,
-} from './counterSlice';
+import PropTypes from 'prop-types';
+import { update, countDown } from './counterSlice';
 import { winLottery } from '../lottery/lotterySlice';
 import {
   CounterOuterContainer, CounterTiTle, CounterInputMins,
@@ -11,38 +12,83 @@ import {
   CounterMins, CounterStart, CounterTimer,
 } from './Counter.style';
 
-export default function Counter() {
+const CounterDisplay = memo(({
+  start, setStart,
+}) => {
   const navigate = useNavigate();
-  const totalSeconds = useSelector((state) => state.counter.totalSeconds);
-  const [mins, setMins] = useState(0);
-  const [start, setStart] = useState(false);
-  const timer = useRef();
   const dispatch = useDispatch();
-  const handleChangeTimer = (e) => {
-    setStart(false);
-    if (e.target.value > 99) {
-      alert('最大數字為99');
-      setMins(99);
-      dispatch(update(99 * 60));
-    } else if (e.target.value > 0) {
-      setMins(parseInt(e.target.value, 10));
-      dispatch(update(parseInt(e.target.value, 10) * 60));
-    }
-  };
+  const totalSeconds = useSelector((state) => state.counter.totalSeconds);
+
   const timeFormat = (num) => (num > 9 ? num : `0${num}`);
+  const minutes = timeFormat(Math.floor(totalSeconds / 60));
+  const seconds = timeFormat(totalSeconds % 60);
+
   useEffect(() => {
+    let interval = null;
     if (start && totalSeconds > 0) {
-      timer.current = setTimeout(() => {
+      interval = setInterval(() => {
         dispatch(countDown());
       }, 1000);
-    } else if (start) {
-      setMins(0);
-      setStart(false);
+    } else if (totalSeconds === 0 && start) {
+      clearInterval(interval);
       dispatch(winLottery(Math.floor(Math.random() * 100)));
       navigate('/result');
+      setStart(false);
     }
-    return (() => { clearTimeout(timer.current); });
-  }, [totalSeconds, start, dispatch, navigate]);
+    return () => clearInterval(interval);
+  }, [start, totalSeconds, dispatch, navigate, setStart]);
+
+  return (
+    <CounterTimer>
+      {minutes}
+      :
+      {seconds}
+    </CounterTimer>
+  );
+});
+
+CounterDisplay.propTypes = {
+  start: PropTypes.bool.isRequired,
+  setStart: PropTypes.func.isRequired,
+};
+
+const MemoizedCounterDisplay = memo(CounterDisplay);
+
+const StartButton = memo(({ mins, setStart }) => {
+  const handleStart = useCallback(() => {
+    if (mins > 0) {
+      setStart(true);
+    }
+  }, [mins, setStart]);
+
+  return (
+    <CounterStartContainer>
+      <CounterStart type="button" onClick={handleStart}>設定</CounterStart>
+    </CounterStartContainer>
+  );
+});
+
+StartButton.propTypes = {
+  mins: PropTypes.number.isRequired,
+  setStart: PropTypes.func.isRequired,
+};
+
+function Counter() {
+  const [mins, setMins] = useState(0);
+  const [start, setStart] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleChangeTimer = useCallback((e) => {
+    const newMins = parseInt(e.target.value, 10);
+    if (Number.isNaN(newMins) || newMins <= 0) {
+      setMins(0);
+      dispatch(update(0));
+    } else {
+      const minutes = Math.min(newMins, 99);
+      setMins(minutes);
+      dispatch(update(minutes * 60));
+    }
+  }, [dispatch]);
 
   return (
     <CounterOuterContainer>
@@ -53,13 +99,13 @@ export default function Counter() {
         max="99"
         onChange={handleChangeTimer}
       />
-      <CounterMinuteContainer><CounterMins>分鐘</CounterMins></CounterMinuteContainer>
-      <CounterStartContainer><CounterStart type="button" onClick={() => { setStart(true); }}>設定</CounterStart></CounterStartContainer>
-      <CounterTimer>
-        {timeFormat(Math.floor(totalSeconds / 60))}
-        :
-        {timeFormat(totalSeconds % 60)}
-      </CounterTimer>
+      <CounterMinuteContainer>
+        <CounterMins>分鐘</CounterMins>
+      </CounterMinuteContainer>
+      <StartButton mins={mins} setStart={setStart} />
+      <MemoizedCounterDisplay start={start} setStart={setStart} />
     </CounterOuterContainer>
   );
 }
+
+export default Counter;
